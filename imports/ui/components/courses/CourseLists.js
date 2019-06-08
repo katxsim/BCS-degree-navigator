880c77b35cd9674d5788f68a059880734c7a7e55
Danny Israel <danny.israel@gmail.com>
2019-06-08 00:00:38 -0700

    added course list section

--- imports/ui/components/courses/CourseLists.js
+++ imports/ui/components/courses/CourseLists.js
@@ -3,8 +3,56 @@ import React from 'react'
 
 class CourseList extends React.component {
     render(){
-        return{
-            
-        }
+        const courses = [
+            {"dept":"CPSC", "num":110},
+            {"dept":"CPSC", "num":210},
+            {"dept":"MATH", "num":200},
+        ]
+        const bridging = [
+            {"dept":"STAT", "num":302}
+        ]
+        const postCourses = courses.length ? (
+            courses.map(course => {
+                return (
+                    <div className="courses container">
+                        <ul className="collection">
+                            <li className="collection-item">
+                                {course.dept} ": " {course.num}
+                                <i className="small materialized-icons">clear</i>
+                            </li>
+                        </ul>
+                    </div>
+                )
+            })
+        ) : ( <div className="courses container">
+                <h2>no courses</h2>
+              </div>)
+
+        const postBridging = bridging.length ? (
+            bridging.map(course => {
+                return (
+                    <div className="courses container">
+                        <ul className="collection">
+                            <li className="collection-item">
+                                {course.dept} ": " {course.num}
+                                <i className="small materialized-icons">clear</i>
+                            </li>
+                        </ul>
+                    </div>
+                )
+            })
+        ) : ( <div className="courses container">
+                <h2 className="center">no courses</h2>
+              </div>)
+
+        return (
+            <div className="courses container">
+                <h2 className="center">Courses</h2>
+                { postCourses }
+                <h2 className="center">Bridging</h2>
+                { postBridging }
+            </div>
+        )
     }
 }
+export default CourseList
