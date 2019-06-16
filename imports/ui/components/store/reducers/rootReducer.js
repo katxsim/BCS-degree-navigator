edfb7ba2fef721a86b2d17181edf9926fc752558
Danny Israel <danny.israel@gmail.com>
2019-06-16 10:03:12 -0700

    added courses to root reducer initial state

---
+++ imports/ui/components/store/reducers/rootReducer.js
@@ -0,0 +1,14 @@
+const initState = {
+    core: [{"dept":"CPSC", "num":110},
+            {"dept":"CPSC", "num":210},
+            {"dept":"MATH", "num":200}],
+    bridging: [{"dept":"STAT", "num":302},
+            {"dept":"STAT", "num":305},
+            {"dept":"STAT", "num":306}],
+    exemptions: [{"dept":"ENGL", "num":110},
+                {"dept":"STAT", "num":200},
+                {"dept":"MATH", "num":108}],
+    replacements: [{"dept":"MATH", "num":221},
+                {"dept":"MATH", "num":200},
+                {"dept":"DSCI", "num":100}]
+}
