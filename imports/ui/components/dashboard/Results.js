3e9d4263bbdd21458aedb71b2e653f8e6b2bb118
Danny Israel <danny.israel@gmail.com>
2019-06-21 02:10:29 -0700

    a bit of styling

--- imports/ui/components/dashboard/Results.js
+++ imports/ui/components/dashboard/Results.js
@@ -9,6 +9,7 @@ class Results extends Component {
                 <div className="col s12 card">
 
                     <h5 className="center">Core</h5>
+                    <progress max="11" value="4" className="col s12 core"/>
                     <p className="complete">CPSC 110</p>
                     <p className="complete">CPSC 210</p>
                     <p className="complete">MATH 108</p>
@@ -19,15 +20,14 @@ class Results extends Component {
                     <p className="incomplete">CPSC 213</p>
                     <p className="incomplete">3 out of 3 electives 300+</p>
                     <p className="incomplete">3 out of 3 electives 400+</p>
-                    <progress max="11" value="4" className="col s12 core"/>
 
                     <h5 className="center">Bridging</h5>
-                    <p className="incomplete">2 out of 3</p>
                     <progress max="3" value="2" className="col s12 bridging"/>
+                    <p className="incomplete">2 out of 3</p>
 
                     <h5 className="center">Exemption Replacements</h5>
-                    <p className="complete">all done</p>
                     <progress max="3" value="3" className="col s12 replacements"/>
+                    <p className="complete">all done</p>
                 </div>
             </div>
         )
