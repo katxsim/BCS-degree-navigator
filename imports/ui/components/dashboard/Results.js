f166e577e189f8d88375956a0733f720247e2ff1
Danny Israel <danny.israel@gmail.com>
2019-06-24 01:23:14 -0700

    making progress on course completion parsing. exemption is done, working on bridging

--- imports/ui/components/dashboard/Results.js
+++ imports/ui/components/dashboard/Results.js
@@ -4,68 +4,117 @@ import { connect } from 'react-redux'
 class Results extends Component {
 
     render() {
-        const { courses } = this.props;
+        const { user } = this.props; // 
         // core
         let requirements = [
-            { "type": "core", "dept": ENGL, "num": 100, "status": "incomplete" },
-            { "type": "core", "dept": MATH, "num": 180, "status": "incomplete" },
-            { "type": "core", "dept": STAT, "num": 203, "status": "incomplete" },
-            { "type": "core", "dept": CPSC, "num": 110, "status": "incomplete" },
-            { "type": "core", "dept": CPSC, "num": 121, "status": "incomplete" },
-            { "type": "core", "dept": CPSC, "num": 210, "status": "incomplete" },
-            { "type": "core", "dept": CPSC, "num": 221, "status": "incomplete" },
-            { "type": "core", "dept": COMM, "num": 300, "status": "incomplete" },
-            { "type": "core", "dept": CPSC, "num": 213, "status": "incomplete" },
-            { "type": "core", "dept": CPSC, "num": 310, "status": "incomplete" },
-            { "type": "core", "dept": CPSC, "num": 313, "status": "incomplete" },
-            { "type": "core", "dept": CPSC, "num": 320, "status": "incomplete" },
-            { "type": "elective", "dept": CPSC, "num": 300, "status": "incomplete" },
-            { "type": "elective", "dept": CPSC, "num": 301, "status": "incomplete" },
-            { "type": "elective", "dept": CPSC, "num": 302, "status": "incomplete" },
-            { "type": "elective", "dept": CPSC, "num": 400, "status": "incomplete" },
-            { "type": "elective", "dept": CPSC, "num": 401, "status": "incomplete" },
-            { "type": "elective", "dept": CPSC, "num": 402, "status": "incomplete" }
+            { "type": "core", "dept": "ENGL", "num": 100, "status": "incomplete", "complex": true },
+            { "type": "core", "dept": "MATH", "num": 180, "status": "incomplete", "complex": false },
+            { "type": "core", "dept": "STAT", "num": 203, "status": "incomplete", "complex": false },
+            { "type": "core", "dept": "CPSC", "num": 110, "status": "incomplete", "complex": false },
+            { "type": "core", "dept": "CPSC", "num": 121, "status": "incomplete", "complex": false },
+            { "type": "core", "dept": "CPSC", "num": 210, "status": "incomplete", "complex": false },
+            { "type": "core", "dept": "CPSC", "num": 221, "status": "incomplete", "complex": false },
+            { "type": "core", "dept": "COMM", "num": 300, "status": "incomplete", "complex": true },
+            { "type": "core", "dept": "CPSC", "num": 213, "status": "incomplete", "complex": false },
+            { "type": "core", "dept": "CPSC", "num": 310, "status": "incomplete", "complex": false },
+            { "type": "core", "dept": "CPSC", "num": 313, "status": "incomplete", "complex": false },
+            { "type": "core", "dept": "CPSC", "num": 320, "status": "incomplete", "complex": false },
+            { "type": "elective", "dept": "CPSC", "num": 300, "status": "incomplete", "complex": true },
+            { "type": "elective", "dept": "CPSC", "num": 300, "status": "incomplete", "complex": true },
+            { "type": "elective", "dept": "CPSC", "num": 300, "status": "incomplete", "complex": true },
+            { "type": "elective", "dept": "CPSC", "num": 400, "status": "incomplete", "complex": true },
+            { "type": "elective", "dept": "CPSC", "num": 400, "status": "incomplete", "complex": true },
+            { "type": "elective", "dept": "CPSC", "num": 400, "status": "incomplete", "complex": true },
+            { "type": "bridging", "dept": "", "num": 0, "status": "incomplete", "complex": false },
+            { "type": "bridging", "dept": "", "num": 0, "status": "incomplete", "complex": false },
+            { "type": "bridging", "dept": "", "num": 0, "status": "incomplete", "complex": false },
+            { "type": "bridging", "dept": "", "num": 0, "status": "incomplete", "complex": false },
+            { "type": "bridging", "dept": "", "num": 0, "status": "incomplete", "complex": false }
         ]
 
-        function coreCoursesMatch (userCourse, requiredCourse) {
-            if (userCourse.type === requiredCourse.type &&
-                userCourse.dept === requiredCourse.dept &&
-                userCourse.num === requiredCourse.num) {
-                if (requiredCourse.status === "complete" ||
-                    userCourse.status === "complete") {
-                    alert("can't add same course twice")
-                    return false;
-                } else {
-                    return true;
-                }
-            } 
-            // if here then no match
-            return false;
-                
-
-        }
-        
-        function checkRequirement(userCourse, requiredCourse) {
-            // check specific course requirement
-            if (coreCoursesMatch(userCourse, requiredCourse)) {
-                userCourse.status = "complete"
-                requiredCourse.status = "complete"
-            }
-            // check elective requirements
-            else if (electiveCoursesMatch(userCourse, requiredCourse)) {
-
-            }
-                
-            }
-
-        
-
-        courses.forEach(function (userCourse) {
+        // iterate over completed courses and compare to requirements
+        // runs in O(n^2) can be improved? constant time? hashtable? 
+        user.courses.forEach(function (userCourse) {
             requirements.forEach(function (requiredCourse) {
-                checkRequirement(userCourse, requiredCourse)
+                if (checkRequirement(userCourse, requiredCourse)) {
+                    userCourse.status = "complete"
+                    requiredCourse.status = "complete"
+                }
             })
         });
 
+        function checkRequirement(userCourse, requiredCourse) {
+            if (userCourse.type === "core" ||
+                userCourse.type === "exemption" &&
+                !alreadyComplete(userCourse, requiredCourse)) {
+                checkIfValidCoreAndExemption(userCourse, requiredCourse)
+            }
+            else if
+                (userCourse.type === "elective" &&
+                !alreadyComplete(userCourse, requiredCourse)) {
+                checkIfValidElective(userCourse, requiredCourse)
+            }
+            else if
+                (userCourse.type === "bridging" &&
+                !alreadyComplete(userCourse, requiredCourse)) {
+                checkIfValidBridging(userCourse)
+            }
+            else if
+                // done
+                (userCourse.type === "replacement" &&
+                !alreadyComplete(userCourse, requiredCourse)) {
+                checkIfValidReplacement(userCourse)
+            }
+        }
+
+        function checkIfValidCoreAndExemption(userCourse, requiredCourse) {
+            // TODO
+        }
+
+        function checkIfValidElective(userCourse, requiredCourse) {
+            // TODO
+        }
+
+        function checkIfValidBridging(userCourse) {
+            if (user.bridgingCpscCounter > 2)
+                alert("Warning: too many CPSC bridging courses"); // should never reach here
+            if (userCourse.dept === "CPSC" &&
+                user.bridgingCpscCounter >= 2) {
+                alert("Warning: There are too many CPSC Bridging courses") // should never reach here
+                return false;
+            } else if (userCourse.num >= 300) return true;
+            // if you reach here...
+            return false;
+        }
+
+        // check for course level exemption allowance and if valid, 
+        //remove it from exemption allowance
+        function checkIfValidReplacement(userCourse) {
+            let courseLevel = Math.floor(userCourse.num / 100) * 100;
+            // best case, course level matches an exemption level
+            if (user.exemptionLevels.includes(courseLevel)) {
+                // remove exemption allowance from list since it is being used
+                user.exemptionLevels.splice(user.exemptionLevels.indexOf(courseLevel), 1);
+                return true;
+            } else if
+                // replacement is higher level than needed (surpassing the requirement)
+                (Math.max(user.exemptionLevels) <= courseLevel) {
+                user.exemptionLevels.splice(
+                    user.exemptionLevels.indexOf(Math.max(user.exemptionLevels)), 1);
+                return true;
+            } else { // your replacement is below the exemption level required
+                return false;
+            }
+        }
+
+        function alreadyComplete(userCourse, requiredCourse) {
+            if (requiredCourse.status === "complete" ||
+                userCourse.status === "complete") {
+                alert(" You can't add the same course twice :) ")
+                return true;
+            } else
+                return false;
+        }
         return (
             <div>
                 <h4 className="center">Progress</h4>
@@ -73,7 +122,8 @@ class Results extends Component {
 
                     <h5 className="center">Core</h5>
                     <progress max="18" value="8" className="col s12 core" />
-                    <p className={ENGL100}>ENGL 100</p>
+                    {/* iterate to produce array of divs which can be inserted */}
+                    {/* <p className={ENGL100}>ENGL 100</p>
                     <p className={MATH180}>MATH 180</p>
                     <p className={STAT203}>STAT 203</p>
                     <p className={CPSC110}>CPSC 110</p>
@@ -90,7 +140,7 @@ class Results extends Component {
                     <p className={CPSC302}>elective 300+</p>
                     <p className={CPSC400}>elective 400+</p>
                     <p className={CPSC401}>elective 400+</p>
-                    <p className={CPSC402}>elective 400+</p>
+                    <p className={CPSC402}>elective 400+</p> */}
 
                     <h5 className="center">Bridging</h5>
                     <progress max="5" value="1" className="col s12 bridging" />
@@ -106,7 +156,7 @@ class Results extends Component {
 }
 const mapStateToProps = (state) => {
     return {
-        courses: state.courses
+        user: state.users[0]
     }
 }
 
--- imports/ui/reducers/rootReducer.js
+++ imports/ui/reducers/rootReducer.js
@@ -1,60 +1,65 @@
 const shortid = require('shortid');
 const initState = {
-    "users": [
-
-        { "email": "test1@gmail.com", "firstName": "test1", "lastName":"test1" },
-        { "email": "test2@gmail.com", "firstName": "test2", "lastName":"test2" },
-        { "email": "test3@gmail.com", "firstName": "test3", "lastName":"test3" }
-    ],
-    "courses": [
-            { "type": "core", "dept": "CPSC", "num": 110, "id": shortid.generate() },
-            { "type": "core", "dept": "CPSC", "num": 210, "id": shortid.generate() },
-            { "type": "core", "dept": "ENGL", "num": 110, "id": shortid.generate() },
-            { "type": "core", "dept": "MATH", "num": 200, "id": shortid.generate() },
-            { "type": "bridging", "dept": "STAT", "num": 302, "id": shortid.generate() },
-            { "type": "bridging", "dept": "STAT", "num": 305, "id": shortid.generate() },
-            { "type": "bridging", "dept": "STAT", "num": 306, "id": shortid.generate() },
-            { "type": "exemptions", "dept": "ENGL", "num": 110, "id": shortid.generate() },
-            { "type": "exemptions", "dept": "STAT", "num": 200, "id": shortid.generate() },
-            { "type": "exemptions", "dept": "MATH", "num": 108, "id": shortid.generate() },
-            { "type": "replacements", "dept": "MATH", "num": 221, "id": shortid.generate() },
-            { "type": "replacements", "dept": "MATH", "num": 200, "id": shortid.generate() },
-            { "type": "replacements", "dept": "DSCI", "num": 100, "id": shortid.generate() }
-        ]
-    }
+  "users": [
+    // user 1
+    {
+      "email": "test1@gmail.com",
+      "firstName": "test1",
+      "lastName": "test1",
+      "creditsEarned": 0,
+      "bridgingCpscCounter": 0,
+      "exemptionLevels": [100, 200, 300], // update this when adding exemption and sort it
+      "courses": [
+        { "type": "core", "dept": "CPSC", "num": 110, "id": shortid.generate() },
+        { "type": "core", "dept": "CPSC", "num": 210, "id": shortid.generate() },
+        { "type": "core", "dept": "ENGL", "num": 110, "id": shortid.generate() },
+        { "type": "core", "dept": "MATH", "num": 200, "id": shortid.generate() },
+        { "type": "bridging", "dept": "STAT", "num": 302, "id": shortid.generate() },
+        { "type": "bridging", "dept": "STAT", "num": 305, "id": shortid.generate() },
+        { "type": "bridging", "dept": "STAT", "num": 306, "id": shortid.generate() },
+        { "type": "exemptions", "dept": "ENGL", "num": 110, "id": shortid.generate() },
+        { "type": "exemptions", "dept": "STAT", "num": 200, "id": shortid.generate() },
+        { "type": "exemptions", "dept": "MATH", "num": 180, "id": shortid.generate() },
+        { "type": "replacements", "dept": "MATH", "num": 221, "id": shortid.generate() },
+        { "type": "replacements", "dept": "MATH", "num": 200, "id": shortid.generate() },
+        { "type": "replacements", "dept": "DSCI", "num": 100, "id": shortid.generate() }
+      ] // end courses
+    } // end user
+  ] // end list of users
+} // end initState
 
 const rootReducer = (state = initState, action) => {
     console.log(state)
     if (action.type === 'DELETE_COURSE') {
 
-        let core = state.courses.core.filter(currCourse => {
-            return currCourse.id !== action.course.id
-        });
-        let bridging = state.courses.bridging.filter(currCourse => {
-            return currCourse.id !== action.course.id
-        });
-        let exemptions = state.courses.exemptions.filter(currCourse => {
-            return currCourse.id !== action.course.id
-        });
-        let replacements = state.courses.replacements.filter(currCourse => {
-            return currCourse.id !== action.course.id
-        });
-        console.log("you deleted " + action.course.dept + " " + action.course.num)
-        return {
-            ...state,
-            "courses": { core, bridging, exemptions, replacements }
-        }
+      let core = state.courses.core.filter(currCourse => {
+        return currCourse.id !== action.course.id
+      });
+      let bridging = state.courses.bridging.filter(currCourse => {
+        return currCourse.id !== action.course.id
+      });
+      let exemptions = state.courses.exemptions.filter(currCourse => {
+        return currCourse.id !== action.course.id
+      });
+      let replacements = state.courses.replacements.filter(currCourse => {
+        return currCourse.id !== action.course.id
+      });
+      console.log("you deleted " + action.course.dept + " " + action.course.num)
+      return {
+        ...state,
+        "courses": { core, bridging, exemptions, replacements }
+      }
     }
     if (action.type === 'ADD_USER') {
-        console.log(action.user)
-        let newUsers = [...state.users, action.user]
-        console.log(newUsers)
-        return {
-            ...state,
-            "users": newUsers
-        }
+      console.log(action.user)
+      let newUsers = [...state.users, action.user]
+      console.log(newUsers)
+      return {
+        ...state,
+        "users": newUsers
+      }
     }
-    return state; 
-}
+    return state;
+  }
 
 export default rootReducer
