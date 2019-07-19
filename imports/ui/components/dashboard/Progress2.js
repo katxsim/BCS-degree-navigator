4501b855901e98a9d8b63d6dbaba2b3fbcc6b6d3
codygagnon <cody.a.gagnon@gmail.com>
2019-07-19 13:37:10 -0700

    added tabs to progress component

---
+++ imports/ui/components/dashboard/Progress2.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Header, Progress, Message } from "semantic-ui-react";
+
+class Progress2 extends Component {
+  render() {
+    return (
+      <div>
+        <div className="ui bottom attached segment tab">Progress 2 Display</div>
+      </div>
+    );
+  }
+}
+
+export default Progress2;
