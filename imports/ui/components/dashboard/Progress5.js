abc95e107b736de84a8fe49af3319c23eac49572
codygagnon <cody.a.gagnon@gmail.com>
2019-07-30 15:25:31 -0700

    Added a Session column to the Grades progress display

--- imports/ui/components/dashboard/Progress5.js
+++ imports/ui/components/dashboard/Progress5.js
@@ -3,11 +3,11 @@ import React, { Component } from "react";
 import { Icon, Table } from "semantic-ui-react";
 
 const tableData = [
-  { course: "CPSC 110", grade: 91 },
-  { course: "CPSC 121", grade: 79 },
-  { course: "MATH 180", grade: 73 },
-  { course: "CPSC 210", grade: 84 },
-  { course: "CPSC 213", grade: 80 }
+  { course: "CPSC 110", grade: 91, session: "2018W1" },
+  { course: "CPSC 121", grade: 79, session: "2018W1" },
+  { course: "MATH 180", grade: 73, session: "2018W1" },
+  { course: "CPSC 210", grade: 84, session: "2018W2" },
+  { course: "CPSC 213", grade: 80, session: "2018W2" }
 ];
 
 class Progress5 extends Component {
@@ -49,20 +49,29 @@ class Progress5 extends Component {
               >
                 Course
               </Table.HeaderCell>
+
               <Table.HeaderCell
                 sorted={column === "grade" ? direction : null}
                 onClick={this.handleSort("grade")}
               >
                 Grade
               </Table.HeaderCell>
+
+              <Table.HeaderCell
+                sorted={column === "session" ? direction : null}
+                onClick={this.handleSort("session")}
+              >
+                Session
+              </Table.HeaderCell>
             </Table.Row>
           </Table.Header>
 
           <Table.Body>
-            {_.map(data, ({ course, grade }) => (
+            {_.map(data, ({ course, grade, session }) => (
               <Table.Row key={course}>
                 <Table.Cell>{course}</Table.Cell>
                 <Table.Cell>{grade}</Table.Cell>
+                <Table.Cell>{session}</Table.Cell>
               </Table.Row>
             ))}
           </Table.Body>
