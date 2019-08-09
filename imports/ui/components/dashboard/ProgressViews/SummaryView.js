2c026523f48ab9867517d00c847eb66130071464
codygagnon <cody.a.gagnon@gmail.com>
2019-08-09 13:11:32 -0700

    cleaned up import statements in summary view

--- imports/ui/components/dashboard/ProgressViews/SummaryView.js
+++ imports/ui/components/dashboard/ProgressViews/SummaryView.js
@@ -1,8 +1,6 @@
 import React, { Component } from "react";
-import { Header, Progress, Icon, Table, Message } from "semantic-ui-react";
+import { Header, Progress, Icon, Table } from "semantic-ui-react";
 import { createContainer } from "meteor/react-meteor-data";
-// import _ from "lodash";
-import { userCourses } from "../../../../collections/userCourses";
 import { updateRequirements } from "../../../../ComputeRequirements";
 const shortid = require("shortid");
 
